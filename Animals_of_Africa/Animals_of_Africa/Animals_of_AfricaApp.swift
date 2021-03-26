c38205cefb037726e6c4ac8514c38d25baa53f48
ajkinder <alexjohnkinder@gmail.com>
2021-03-26 16:22:49 -0700

    second commit. Added assests to project. Added mp4 files to project.

---
+++ Animals_of_Africa/Animals_of_Africa/Animals_of_AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animals_of_AfricaApp.swift
+//  Animals_of_Africa
+//
+//  Created by AlexKinder on 3/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Animals_of_AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animals_of_Africa/Animals_of_Africa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animals_of_Africa
+//
+//  Created by AlexKinder on 3/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
